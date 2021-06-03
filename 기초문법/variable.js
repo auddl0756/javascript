d29c37232329fa634619cc9ef1dd1f123e790553
auddl0756 <cjddl789123@naver.com>
2021-06-03 21:41:43 +0900

    data types

---
+++ 기초문법/variable.js
@@ -0,0 +1,25 @@
+// 데이터 타입 : String , Number , Boolean , Null
+// 변수 선언 : var 하나로 다 처리
+
+//String 
+var a="hello";
+var a_tag = "<h1> hello ? </h1>";
+
+// Number 
+var b =111;
+var b2 = Number("222");
+
+//Boolean
+var c=true;
+var c2= 111>=100;
+var c3 =Boolean("hello");
+
+//Null
+var d =null;
+
+//typeof 
+document.writeln(typeof a);
+document.writeln(typeof b);
+document.writeln(typeof b2);
+document.writeln(typeof c);
+document.writeln(typeof d);
